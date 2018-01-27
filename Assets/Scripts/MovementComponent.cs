2f1367cc8d9fb30264548c1eece01db7c2339de4
Manuel Pacheco <manuelalejandropm@gmail.com>
2018-01-26 22:55:41 -0400

    initial files

---
+++ Assets/Scripts/MovementComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementComponent : MonoBehaviour {
+	public float speed;
+  public float rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	void Update () {
+		// Forward movement
+		var direction = transform.forward * speed;
+		transform.Translate(direction);
+
+		// Lateral movement
+    float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+		transform.Rotate(0, rotation, 0);
+	}
+}
