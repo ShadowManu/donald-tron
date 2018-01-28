0a230315849afa2bf42bc88d3b681c5297eb95e0
Regiosen <Alenios@hotmail.com>
2018-01-28 12:53:32 -0400

    Mejora del movimiento
    Se agrega el control para usar los gatillos tambien

--- Assets/Scripts/MovementComponent.cs
+++ Assets/Scripts/MovementComponent.cs
@@ -19,25 +19,18 @@ public class MovementComponent : MonoBehaviour {
 
 		// Lateral movement
 		float rotation = Input.GetAxis("Horizontal" + " " + axisSuffix) * rotationSpeed;
-        //t.eulerAngles = new Vector3(t.eulerAngles.x, t.eulerAngles.y + 0 + rotation*5 , t.eulerAngles.z);
-        transform.Rotate(0, rotation, 0);
+                Debug.Log(rotation);
+		// Lateral movement (gatillo overwrite)
+                 
+		float rotationGat = Input.GetAxis("Gatillos" + " " + axisSuffix) * rotationSpeed;
+                Debug.Log(rotationGat);
+                if (rotationGat != 0){
+                    rotation = -rotationGat;
+                }
+		//t.eulerAngles = new Vector3(t.eulerAngles.x, t.eulerAngles.y + 0 + rotation*5 , t.eulerAngles.z);
+		transform.Rotate(0, rotation, 0);
 
-        RaycastHit hitInfo;
-
-        if (Physics.Raycast(transform.position, transform.forward, out hitInfo, 1)){
-
-            Debug.Log(hitInfo.collider.name);
-
-            if (hitInfo.collider.name != "Pared")
-            {
-                // Forward movement
-                transform.position += transform.forward * Time.deltaTime * speed;
-            }
-        }else
-        {
-            // Forward movement
-            transform.position += transform.forward * Time.deltaTime * speed;
-        }
-    }
-
+		// Forward movement
+		transform.position += transform.forward * Time.deltaTime * speed;
+	}
 }
